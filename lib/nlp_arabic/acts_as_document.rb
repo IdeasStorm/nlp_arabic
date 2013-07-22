0ce18d69b03d8e5c894aec01f55104fc941583f4
Yaman ALTereh <m.y.t-91@hotmail.com>
2013-07-22 23:18:23 +0300

    added methods in acts_as_doc

--- lib/nlp_arabic/acts_as_document.rb
+++ lib/nlp_arabic/acts_as_document.rb
@@ -14,6 +14,61 @@ module NlpArabic
     end
 
     module LocalInstanceMethods
+
+      def self.get_root_words (words)
+        all_terms = []
+        words.each do |w|
+          term = ArabicStemmer.get_instance.format_sentence(w)
+          if term != "" and !term.nil?
+            all_terms << term
+          end
+        end
+        #  	word.each do |w|
+        #  		temp = RootTerms.find(w)
+        #  		if temp.nil?
+        # get word's root
+        #r_w = get_root_term w
+        #RootTerms.create(:word => w, :root => r_w)
+        #  			end
+        #  		elseif (!temp.root.nil?)
+        #  			r_w = temp.root
+        #  		end
+        #  		all_terms << r_w
+        #  	end #for word.each do |w|
+        return all_terms.uniq
+      end
+
+      private
+      def self.calculate_term_frequencies(terms)
+        tf = {}
+        max_frequency_of_terms = 0
+        terms.each do |term|
+          if !tf[term].nil?
+            tf[term] += 1
+          else
+            tf[term] = 1
+          end
+          if tf[term] > max_frequency_of_terms
+            max_frequency_of_terms = tf[term]
+          end
+        end
+
+        tf.each_pair { |k,v| tf[k] = (0.5 + (( 0.5 * v.to_f) / max_frequency_of_terms))  }
+        return tf
+      end
+
+      private
+      def save_terms(terms)
+        terms.uniq.each do |t|
+          term = Term.find_by_word(t)
+          if term.nil?
+            Term.create(:word => t, :doc_freq => 1)
+          else
+            term.update_attributes(:doc_freq => term.doc_freq+1)
+          end
+        end
+      end
+
       def get_related_documents
         #Todo add logic here
       end
