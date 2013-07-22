a0254877f2a9bdcdcef986c27d90743bdbe85f2d
Mohammed Aljuboori <mhdaljobory@gmail.com>
2013-07-23 01:45:47 +0300

    Added get_synonyms_root method

--- lib/nlp_arabic/arabic_wordnet.rb
+++ lib/nlp_arabic/arabic_wordnet.rb
@@ -3,12 +3,12 @@ class ArabicWordnet < ActiveRecord::Base
   
   ###
   ##  return array of synonyms of input word
-  ##  output:
+  ##  input:
   ##    word: input word
   ##    root:
   ##      true  => the input word is a root
   ##      false => the input word is not a root
-  ##  input:
+  ##  output:
   ##    array of synonyms not root words
   ###
   def self.get_synonyms(word, root)
@@ -45,6 +45,66 @@ class ArabicWordnet < ActiveRecord::Base
                           INNER JOIN items t1
                           ON w1.synsetid = t1.itemid
                           AND t1.name = '#{word}'")
+      end
+    end
+    # return flatten array
+    words.collect {|word| word[0]}
+  end
+  
+  ###
+  ##  return array of synonyms of input word
+  ##  input:
+  ##    word: input word
+  ##    root:
+  ##      true  => the input word is a root
+  ##      false => the input word is not a root
+  ##  output:
+  ##    array of synonyms root words
+  ###
+  def self.get_synonyms_root(word, root)
+    words = Array.new
+    db = connection
+
+    # If the input word is a root
+    if (root)
+      # Find the words in form table
+      words = db.execute("SELECT Distinct f1.value
+                          FROM forms f1
+                          WHERE f1.wordid IN
+                                     (SELECT Distinct w1.wordid
+                                      FROM words w1
+                                      INNER JOIN (SELECT w2.synsetid
+                                                  FROM words w2
+                                                  WHERE w2.wordid IN 
+                                                          (SELECT f2.wordid
+                                                            FROM forms f2
+                                                            WHERE f2.value = '#{word}')) AS res
+                                      ON w1.synsetid = res.synsetid)")
+    end
+
+    # If there are no result
+    if (words.count < 1)
+      # Find the words in words table
+      words = db.execute("SELECT Distinct f1.value
+                          FROM forms f1
+                          WHERE f1.wordid IN
+                                     (SELECT w1.wordid
+                                      FROM words w1
+                                      INNER JOIN words w2
+                                      ON w1.synsetid = w2.synsetid
+                                      AND w2.value = '#{word}')")
+
+      # If there are no result
+      if (words.count < 1)
+        # Find the words in items table
+        words = db.execute("SELECT Distinct f1.value
+                            FROM forms f1
+                            WHERE f1.wordid IN
+                                        (SELECT w1.wordid
+                                        FROM words w1
+                                        INNER JOIN items t1
+                                        ON w1.synsetid = t1.itemid
+                                        AND t1.name = '#{word}')")
       end
     end
     # return flatten array
