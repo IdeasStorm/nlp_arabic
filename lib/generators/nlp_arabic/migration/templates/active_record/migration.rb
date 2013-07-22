41b1eaa51c1f85fb828015e02a96e4f556404f5d
Yaman ALTereh <m.y.t-91@hotmail.com>
2013-07-22 22:52:37 +0300

    added migrations terms and freq_term_in_doc

--- lib/generators/nlp_arabic/migration/templates/active_record/migration.rb
+++ lib/generators/nlp_arabic/migration/templates/active_record/migration.rb
@@ -1,3 +1,19 @@
 class ActsAsDocumentMigration < ActiveRecord::Migration
   #TODO put migration logic here
+
+  create_table :terms do |t|
+    t.string :word
+    t.integer :doc_freq
+
+    t.timestamps
+  end
+
+  create_table :freq_term_in_docs do |t|
+    t.integer :doc_id
+    t.string :word
+    t.float :freq
+
+    t.timestamps
+  end
+
 end
