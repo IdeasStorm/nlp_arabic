c59523981976b8e05ce66ffb2195b3bbbe134fe2
Yaman ALTereh <m.y.t-91@hotmail.com>
2013-07-23 23:20:01 +0300

    fixed requir

--- lib/nlp_arabic.rb
+++ lib/nlp_arabic.rb
@@ -1,5 +1,8 @@
 require 'nlp_arabic/arabic_stemmer'
 require 'nlp_arabic/arabic_wordnet'
 require 'nlp_arabic/acts_as_document'
+require 'nlp_arabic/freq_term_in_doc'
+require 'nlp_arabic/term'
+
 module NlpArabic
 end
