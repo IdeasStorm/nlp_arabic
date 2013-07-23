d10552c6d359a92cd8017880f2b2d7affe045587
Yaman ALTereh <m.y.t-91@hotmail.com>
2013-07-23 23:52:43 +0300

    fixed static methods

--- lib/nlp_arabic/acts_as_document.rb
+++ lib/nlp_arabic/acts_as_document.rb
@@ -4,7 +4,7 @@ module NlpArabic
 
     @@registered_classes = Array.new
     #TODO check it
-    #after_save :add_document
+
 
     module ClassMethods
       def acts_as_document(options={})
@@ -21,26 +21,7 @@ module NlpArabic
         puts 'worked'
       end
 
-      #TODO put class methods here
-    end
-
-    module LocalInstanceMethods
-
-      def add_document
-        words = read_attribute(self.class.acts_as_document_field)
-        words = words.split
-        all_terms = LocalInstanceMethods::get_root_words(words)
-        #if self.has_attributes?('root_terms')
-        #if self.class.exist?('root_terms')
-        if respond_to?('root_terms')
-          self.write_attributes(:root_terms => all_terms.join(" "))
-        end
-        terms_freq = LocalInstanceMethods::calculate_term_frequencies(all_terms)
-        save_tf (terms_freq)
-        save_term(all_terms)
-      end
-
-      def self.get_root_words (words)
+      def get_root_words (words)
         all_terms = []
         words.each do |w|
           term = ArabicStemmer.get_instance.format_sentence(w)
@@ -63,8 +44,7 @@ module NlpArabic
         return all_terms.uniq
       end
 
-      private
-      def self.calculate_term_frequencies(terms)
+      def calculate_term_frequencies(terms)
         tf = {}
         max_frequency_of_terms = 0
         terms.each do |term|
@@ -80,6 +60,101 @@ module NlpArabic
 
         tf.each_pair { |k,v| tf[k] = (0.5 + (( 0.5 * v.to_f) / max_frequency_of_terms))  }
         return tf
+      end
+
+      #To used it for query
+      def document_freq_of_terms(terms)
+        df = {}
+        terms.uniq.each do |t|
+          term = Term.find_by_word(t)
+          if term.nil?
+            df[t] = 0
+          else
+            df[t] = term.doc_freq
+          end
+        end
+        return df
+      end
+
+      def weights (tf,syn_hash=nil)
+        w = {}
+        docs_count = self.class.count
+        df = self.document_freq_of_terms(tf.keys)
+        tf.each_pair { |term,freq|
+          w[term] = (freq *  Math.log(docs_count.to_f / (1+ df[term]) ))
+          if !syn_hash.nil?
+            if !syn_hash[term].nil?
+              w[term] *= syn_hash[term]
+            end
+          end
+        }
+        return w
+      end
+
+      private
+      def lenght_vector (weights)
+        lenght = 0
+        weights.each_pair { |t,w|
+          lenght += ( w * w)
+        }
+        return Math.sqrt(lenght)
+      end
+
+      private
+      def sim (v1, v2)
+        sum = 0
+        v1.each_pair { |t,w|
+          if !v2[t].nil?
+            sum += (v1[t]*v2[t])
+          end
+        }
+
+        l1 = lenght_vector(v1)
+        l2 = lenght_vector(v2)
+        return sum / (l1 * l2)
+      end
+
+      def similarity (query,num=0,synonyms=true)
+        sims = {}
+        syn_hash = {}
+        terms_q = self.get_root_words(query.split) #return array
+        if synonyms
+          terms_q.each do |t|
+            syns = ArabicStemmer.get_instance.get_synonyms(t)
+            if !syns.nil?
+              syn_hash.merge!(syns)
+            end
+          end
+          terms_q << syn_hash.keys
+          terms_q = terms_q.join(" ").split.uniq
+        end
+        tf_q = self.calculate_term_frequencies(terms_q)
+        w_q = self.weights(tf_q,syn_hash)
+                                                   #TODO !!
+        docs = self.class.all
+        docs.each do |doc|
+          temp = self.sim(w_q, doc.weights)
+          if temp > 0
+            sims[doc] = temp
+          end
+        end
+        return Hash[sims.sort_by{|k, v| v}.reverse].first ((num ==0)? sims.count : num)
+      end
+
+    end
+
+    module LocalInstanceMethods
+
+      def add_document
+        words = read_attribute(self.class.acts_as_document_field)
+        words = words.split
+        all_terms = self.class.get_root_words(words)
+        if respond_to?('root_terms')
+          self.write_attributes(:root_terms => all_terms.join(" "))
+        end
+        terms_freq = self.class.calculate_term_frequencies(all_terms)
+        save_tf (terms_freq)
+        save_terms(all_terms)
       end
 
       private
@@ -127,20 +202,6 @@ module NlpArabic
         return df
       end
 
-      #To used it for query
-      def self.document_freq_of_terms(terms)
-        df = {}
-        terms.uniq.each do |t|
-          term = Term.find_by_word(t)
-          if term.nil?
-            df[t] = 0
-          else
-            df[t] = term.doc_freq
-          end
-        end
-        return df
-      end
-
       def weights
         w = {}
         tf = self.term_frequencies
@@ -153,43 +214,6 @@ module NlpArabic
         return w
       end
 
-      def self.weights (tf,syn_hash=nil)
-        w = {}
-        docs_count = self.class.count
-        df = self.document_freq_of_terms(tf.keys)
-        tf.each_pair { |term,freq|
-          w[term] = (freq *  Math.log(docs_count.to_f / (1+ df[term]) ))
-          if !syn_hash.nil?
-            if !syn_hash[term].nil?
-              w[term] *= syn_hash[term]
-            end
-          end
-        }
-        return w
-      end
-
-      private
-      def self.lenght_vector (weights)
-        lenght = 0
-        weights.each_pair { |t,w|
-          lenght += ( w * w)
-        }
-        return Math.sqrt(lenght)
-      end
-
-      private
-      def self.sim (v1, v2)
-        sum = 0
-        v1.each_pair { |t,w|
-          if !v2[t].nil?
-            sum += (v1[t]*v2[t])
-          end
-        }
-
-        l1 = lenght_vector(v1)
-        l2 = lenght_vector(v2)
-        return sum / (l1 * l2)
-      end
 
       def similarity_with_doc (doc2)
         w1 = self.weights
@@ -197,32 +221,7 @@ module NlpArabic
         return self.sim(w1, w2)
       end
 
-      def self.similarity (query,num=0,synonyms=true)
-        sims = {}
-        syn_hash = {}
-        terms_q = self.get_root_words(query.split) #return array
-        if synonyms
-          terms_q.each do |t|
-            syns = ArabicStemmer.get_instance.get_synonyms(t)
-            if !syns.nil?
-              syn_hash.merge!(syns)
-            end
-          end
-          terms_q << syn_hash.keys
-          terms_q = terms_q.join(" ").split.uniq
-        end
-        tf_q = self.calculate_term_frequencies(terms_q)
-        w_q = self.weights(tf_q,syn_hash)
-        #TODO !!
-        docs = self.class.all
-        docs.each do |doc|
-          temp = self.sim(w_q, doc.weights)
-          if temp > 0
-            sims[doc] = temp
-          end
-        end
-        return Hash[sims.sort_by{|k, v| v}.reverse].first ((num ==0)? sims.count : num)
-      end
+
 
       def get_related_documents(num=5)
         res = {}
