6ca56cc07b9f5ab8434a18a641275f03624297d1
Mohammed Aljuboori <mhdaljobory@gmail.com>
2013-07-23 18:03:02 +0300

    Added new task to download wordnet db

---
+++ lib/tasks/arabic_wordnet_db.rb
@@ -0,0 +1,11 @@
+# Download Arabic Wordnet DB and extract it to db folder
+
+ZIP_ArWN_URL = "http://media.ideasstorm.net/ArabicWordnetDB/ArabicWordnet.zip"
+ZIP_LOC = "../../db/ArabicWordnet.zip"
+
+UNZIP_LOC = "../../db"
+UNZIP_FILE_NAME = "ArabicWordnet.sqlite"
+
+task :download_db do
+  
+end
