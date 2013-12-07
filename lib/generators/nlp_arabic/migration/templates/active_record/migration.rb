c4fc2daf707a3c8ad17e4dceeb09f1cc580ef830
Yaman ALTereh <m.y.t-91@hotmail.com>
2013-12-07 22:04:58 +0200

    fixed migration and update_attribute

--- lib/generators/nlp_arabic/migration/templates/active_record/migration.rb
+++ lib/generators/nlp_arabic/migration/templates/active_record/migration.rb
@@ -4,7 +4,7 @@ class ActsAsDocumentMigration < ActiveRecord::Migration
   def self.up
     create_table :terms do |t|
       t.string :word
-      t.integer :doc_freq
+      t.integer :doc_freq, :default => 0
 
       t.timestamps
     end
@@ -13,14 +13,14 @@ class ActsAsDocumentMigration < ActiveRecord::Migration
     create_table :freq_term_in_docs do |t|
       t.integer :doc_id
       t.string :word
-      t.float :freq
+      t.float :freq, :default => 0
 
       t.timestamps
     end
     add_index :freq_term_in_docs, [:doc_id, :word], :unique => true
 
     NlpArabic::ActsAsDocument.registered_classes.each do |c|
-      add_column (c.downcase+"s").to_sym, :root_terms, :string
+      add_column (c.downcase+"s").to_sym, :root_terms, :string, :default => ''
     end
   end
 
